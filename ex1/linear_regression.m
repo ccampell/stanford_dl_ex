75dc61aba72c42b5b8341049f37683c6aa821411
Chris Campell <ccampell.asu@gmail.com>
2016-08-31 15:29:28 -0400

    Added working implementation of linear and logistic regression.

--- ex1/linear_regression.m
+++ ex1/linear_regression.m
@@ -1,4 +1,4 @@
-function [f,g] = linear_regression(theta, X,y)
+function [f,g] = linear_regression(theta, X, y)
   %
   % Arguments:
   %   theta - A vector containing the parameter values to optimize.
@@ -21,4 +21,24 @@ function [f,g] = linear_regression(theta, X,y)
   %        the examples in X and adding up the gradient for each example.  Store the
   %        computed gradient in 'g'.
   
-%%% YOUR CODE HERE %%%
+
+%%% Compute the Linear Regression Objective/Cost %%%
+% Dot product Theta Transpose with every feature column of matrix X
+for i = 1:m
+    x_i = X(:,i);
+    % Take the dot product of column x^{i} from 'train.x' with weight vector
+    % \theta^{T} to produce the scalar estimate. 
+    % estimate = dot(x_i,theta.');
+    estimate = theta' * x_i;
+    % Sum the cost/objective over every column of x^{i} and store in 'f'.
+    f = f + (estimate - y(i))^2;
+    % Iterate over every parameter theta_{j} to compute the gradient.
+    for j = 1:n
+        g_j = (estimate - y(i)) * x_i(j);
+        % Update the gradient vector 'g' by mupltiplying the j'th element of
+        % column vector x^{i} by predicted. 
+        g(j) = g(j) + g_j;
+    end
+end
+% Multiply the cost/objective 'f' by 1/2.
+f = times(f,.5);
--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -6,13 +6,12 @@ function [f,g] = logistic_regression(theta, X,y)
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
   %
-
-  m=size(X,2);
+  n = size(X,1);
+  m = size(X,2);
   
   % initialize objective value and gradient.
   f = 0;
   g = zeros(size(theta));
-
 
   %
   % TODO:  Compute the objective function by looping over the dataset and summing
@@ -21,4 +20,18 @@ function [f,g] = logistic_regression(theta, X,y)
   % TODO:  Compute the gradient of the objective by looping over the dataset and summing
   %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
   %
-%%% YOUR CODE HERE %%%
+  %%% Compute the objective/cost function %%%
+  % Iterate over every column vector in train.X to 
+  for i = 1:m
+      x_i = X(:,i);
+      z = theta'*x_i; 
+      sigmoid = (1 / (1 + exp(-z)));
+      estimate = y(i)*(log(sigmoid)) + (1-y(i))*log(1-sigmoid);
+      f = f + estimate;
+      for j = 1:n
+        g_j = x_i(j) * (sigmoid - y(i));
+        g(j) = g(j) + g_j;
+      end
+  end
+  f = -f;
+  
